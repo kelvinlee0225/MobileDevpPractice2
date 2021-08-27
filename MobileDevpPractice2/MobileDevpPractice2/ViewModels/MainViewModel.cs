462acab679710406d1d183dc7bd837e088185e6e
kelvinlee0225 <leekelvin0225@gmail.com>
2021-08-27 02:00:36 -0400

    Added MainViewModel Codebehind

--- MobileDevpPractice2/MobileDevpPractice2/ViewModels/MainViewModel.cs
+++ MobileDevpPractice2/MobileDevpPractice2/ViewModels/MainViewModel.cs
@@ -9,10 +9,20 @@ namespace MobileDevpPractice2.ViewModels
     {
         public User user { get; set; } = new User();
         public MenuOption BarAndHotelsMenuOption { get; set; } = new MenuOption();
+        public MenuOption FineDiningMenuOption { get; set; } = new MenuOption();
+        public MenuOption CafesMenuOption { get; set; } = new MenuOption();
+        public MenuOption NearbyMenuOption { get; set; } = new MenuOption();
+        public MenuOption FastFoodsMenuOption { get; set; } = new MenuOption();
+        public MenuOption FeaturedFoodsMenuOption { get; set; } = new MenuOption();
 
         public MainViewModel()
         {
-            BarAndHotelsMenuOption.Title = "Bar And Hotels";
+            BarAndHotelsMenuOption = new MenuOption() { Title = "Bar And Hotels", TotalPlaces="42 Places", Image= "BarAndHotels.png" };
+            FineDiningMenuOption = new MenuOption() { Title = "Fine Dining", TotalPlaces = "42 Places", Image = "FineDining.png" };
+            CafesMenuOption = new MenuOption() { Title = "Cafes", TotalPlaces = "42 Places", Image = "Cafes.png" };
+            NearbyMenuOption = new MenuOption() { Title = "Nearby", TotalPlaces = "42 Places", Image = "Nearby.png" };
+            FastFoodsMenuOption = new MenuOption() { Title = "Fast Foods", TotalPlaces = "42 Places", Image = "FastFoods.png" };
+            FeaturedFoodsMenuOption = new MenuOption() { Title = "Featured Foods", TotalPlaces = "42 Places", Image = "FeaturedFoods.png" };
         }
     }
 }
