b7ac8f7e6d7fe4c6ffd792ae5c3ecccc892b7bca
kelvinlee0225 <leekelvin0225@gmail.com>
2021-08-27 01:54:53 -0400

    Added Views

--- MobileDevpPractice2/MobileDevpPractice2/App.xaml.cs
+++ MobileDevpPractice2/MobileDevpPractice2/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MobileDevpPractice2
         {
             InitializeComponent();
 
-            MainPage = new MainTabbedPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
